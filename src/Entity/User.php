dd06df816f60963a1370cc77d250b59144cd8894
bastien.cornu <bastien.cornu@info0727.iut-lens.univ-artois.fr>
2017-12-18 13:44:00 +0100

    feat(entites): Ajout des regles de validation et creation des entites

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
@@ -20,48 +21,25 @@ class User implements UserInterface, \Serializable
 
     /**
      * @ORM\Column(type="string", unique=true)
+     * @Assert\Email()
      */
     private $email;
 
     /**
+     *
      * @ORM\Column(type="string")
-     */
-    private $firstname;
-
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $lastname;
-
-    /**
-     * @ORM\Column(type="date")
-     */
-    private $birthday;
-
-    /**
-     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     * @Assert\Length(min="5", max="10")
      */
     private $password;
-
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
-
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $updatedAt;
 
     /**
      * @ORM\Column(type="boolean")
      */
     private $isAdmin = false;
 
-    public function __construct()
-    {
-        $this->createdAt = $this->updatedAt = new \DateTime();
-    }
+
+
 
     public function getId() : ?int
     {
@@ -78,36 +56,6 @@ class User implements UserInterface, \Serializable
         $this->email = $email;
     }
 
-    public function getFirstname() : ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(?string $firstname) : void
-    {
-        $this->firstname = $firstname;
-    }
-
-    public function getLastname() : ?string
-    {
-        return $this->lastname;
-    }
-
-    public function setLastname(?string $lastname) : void
-    {
-        $this->lastname = $lastname;
-    }
-
-    public function getBirthday() : ?\DateTime
-    {
-        return $this->birthday;
-    }
-
-    public function setBirthday(\DateTime $birthday) : void
-    {
-        $this->birthday = $birthday;
-    }
-
     public function getPassword() : ?string
     {
         return $this->password;
@@ -118,30 +66,16 @@ class User implements UserInterface, \Serializable
         $this->password = $password;
     }
 
-    public function getCreatedAt() : \DateTime
-    {
-        return $this->createdAt;
-    }
-
-    public function getUpdatedAt() : \DateTime
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTime $updatedAt) : void
-    {
-        $this->updatedAt = $updatedAt;
-    }
-
     public function isAdmin() : bool
     {
         return $this->isAdmin;
     }
-
     public function setIsAdmin(bool $isAdmin) : void
     {
         $this->isAdmin = $isAdmin;
     }
+
+
 
     /**
      * {@inheritdoc}
@@ -187,9 +121,6 @@ class User implements UserInterface, \Serializable
         return serialize([
             $this->id,
             $this->email,
-            $this->firstname,
-            $this->lastname,
-            $this->isAdmin,
             $this->password,
         ]);
     }
@@ -202,9 +133,6 @@ class User implements UserInterface, \Serializable
         list(
             $this->id,
             $this->email,
-            $this->firstname,
-            $this->lastname,
-            $this->isAdmin,
             $this->password) = unserialize($serialized);
     }
 }
---
+++ src/Entity/WeaponSkin.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bastien.cornu
+ * Date: 18/12/17
+ * Time: 13:11
+ */
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tp_weapon_skin")
+ */
+class WeaponSkin
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updateAt;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $beauty;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="decimal", precision=5, scale=2)
+     */
+    private $price;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     */
+    private $user;
+
+    //CONSTRUCTEURS
+    public function __construct()
+    {
+        $this->createdAt = $this->updatedAt = new \DateTime();
+    }
+
+    //GETTERS && SETTERS
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdateAt()
+    {
+        return $this->updateAt;
+    }
+
+    /**
+     * @param mixed $updateAt
+     */
+    public function setUpdateAt($updateAt)
+    {
+        $this->updateAt = $updateAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBeauty()
+    {
+        return $this->beauty;
+    }
+
+    /**
+     * @param mixed $beauty
+     */
+    public function setBeauty($beauty)
+    {
+        $this->beauty = $beauty;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+
+}
